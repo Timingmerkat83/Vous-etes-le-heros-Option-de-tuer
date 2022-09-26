f4c135e4c2f1b6c1bbda3f63f4f52384584b26e3
SparkyPX <pierrelucproulx@gmail.com>
2022-09-26 11:47:22 -0400

    started story in js

---
+++ script.js
@@ -0,0 +1,81 @@
+let chaptersObj = {
+    chapitre1: function goToChapter(){
+        console.log(chapitre1.subtitle, chapitre1.text);
+    },
+
+    chapitre2: function goToChapter(){
+        console.log(chapitre2.subtitle, chapitre2.text);
+    },
+
+    chapitre3: function goToChapter(){
+        console.log(chapitre3.subtitle, chapitre3.text);
+    },
+
+    chapitre4: function goToChapter(){
+        console.log(chapitre4.subtitle, chapitre4.text);
+    },
+
+    chapitre5: function goToChapter(){
+        console.log(chapitre5.subtitle, chapitre5.text);
+    },
+
+    chapitre6: function goToChapter(){
+        console.log(chapitre6.subtitle, chapitre6.text);
+    },
+
+    chapitre7: function goToChapter(){
+        console.log(chapitre7.subtitle, chapitre7.text);
+    },
+
+}
+
+let chapitre1 = {
+    subtitle: "Le début de l'histoire",
+    text: "Il est 7h30, et vous êtes confortable dans votre lit douillet. Votre alarme sonne, que faites-vous?",
+}
+
+let chapitre2 = {
+    subtitle:"",
+    text: "",
+}
+
+let chapitre3 = {
+    subtitle:"",
+    text: "",
+}
+
+let chapitre4 = {
+    subtitle:"",
+    text: "",
+}
+
+let chapitre5 = {
+    subtitle:"",
+    text: "",
+}
+
+let chapitre6 = {
+    subtitle:"",
+    text: "",
+}
+
+let chapitre7 = {
+    subtitle:"",
+    text: "",
+}
+
+let chapitre8 = {
+    subtitle:"",
+    text: "",
+}
+
+let chapitre9 = {
+    subtitle:"",
+    text: "",
+}
+
+
+
+
+
+
