cea0bfe68bdce016937d14df9995fdd37adcb13b
SparkyPX <pierrelucproulx@gmail.com>
2022-09-26 12:53:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
 let chaptersObj = {
-    chapitre1: function goToChapter(){
+
+    prologue: function goToChapter() {
+        console.log(prologue.subtitle, "<br>", prologue.text)
+    },
+
+    chapitre1: function goToChapter() {
         console.log(chapitre1.subtitle, chapitre1.text);
     },
 
-    chapitre2: function goToChapter(){
+    chapitre2: function goToChapter() {
         console.log(chapitre2.subtitle, chapitre2.text);
     },
 
-    chapitre3: function goToChapter(){
+    chapitre3: function goToChapter() {
         console.log(chapitre3.subtitle, chapitre3.text);
     },
 
-    chapitre4: function goToChapter(){
+    chapitre4: function goToChapter() {
         console.log(chapitre4.subtitle, chapitre4.text);
     },
 
-    chapitre5: function goToChapter(){
+    chapitre5: function goToChapter() {
         console.log(chapitre5.subtitle, chapitre5.text);
     },
 
-    chapitre6: function goToChapter(){
+    chapitre6: function goToChapter() {
         console.log(chapitre6.subtitle, chapitre6.text);
     },
 
-    chapitre7: function goToChapter(){
+    chapitre7: function goToChapter() {
         console.log(chapitre7.subtitle, chapitre7.text);
     },
 
+}
+
+let prologue = {
+    subtitle: "Option de tuer",
+    text: "Vous incarnez une jeune adulte sous le nom de Bonnie Parker, elle vie avec son oncle Arthur. Étant témoin du meurtre de ses parents, elle décidèrent de poursuivre ce que son père avait commencé, la recherche d'un homme planifiant de causer la mort de millions de personnes à travers le pays",
 }
 
 let chapitre1 = {
@@ -35,47 +45,41 @@ let chapitre1 = {
 }
 
 let chapitre2 = {
-    subtitle:"",
-    text: "",
+    subtitle: "L'appel",
+    text: "Vous recevez un appel",
 }
 
 let chapitre3 = {
-    subtitle:"",
+    subtitle: "",
     text: "",
 }
 
 let chapitre4 = {
-    subtitle:"",
+    subtitle: "",
     text: "",
 }
 
 let chapitre5 = {
-    subtitle:"",
+    subtitle: "",
     text: "",
 }
 
 let chapitre6 = {
-    subtitle:"",
+    subtitle: "",
     text: "",
 }
 
 let chapitre7 = {
-    subtitle:"",
+    subtitle: "",
     text: "",
 }
 
 let chapitre8 = {
-    subtitle:"",
+    subtitle: "",
     text: "",
 }
 
 let chapitre9 = {
-    subtitle:"",
+    subtitle: "",
     text: "",
 }
-
-
-
-
-
-
