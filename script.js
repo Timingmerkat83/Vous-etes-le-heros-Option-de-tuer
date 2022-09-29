e1f30b1937c0f2cc0a23cbea7fe395db3e6bbae5
SparkyPX <pierrelucproulx@gmail.com>
2022-09-29 12:14:34 -0400

    js more stuff added

--- script.js
+++ script.js
@@ -35,18 +35,22 @@ let chaptersObj = {
 
   badchoice5: function goToChapter() {
     console.log(badchoice5.text);
-  }
+  },
+
+  badchoice6: function goToChapter() {
+    console.log(badchoice6.text);
+  },
 
   ch4altpath: function goToChapter() {
     console.log(ch4path1.subtitle, ch4path1.text);
   },
 
-  chapitre7: function goToChapter() {
-    console.log(chapitre7.subtitle, chapitre7.text);
+  chapitre5: function goToChapter() {
+    console.log(chapitre5.subtitle, chapitre5.text);
   },
 
-chapitre9 : function goToChapter() {
-    console.log(chapitre9.subtitle, chapitre9.text);
+chapitre7 : function goToChapter() {
+    console.log(chapitre7.subtitle, chapitre7.text);
 },
 
 badending : function goToChapter() {
@@ -144,13 +148,14 @@ let chapitre3 = {
   text: "Vous arrivez à destination. Maintenant, vous prenez connaissance aux informations de la mission et du suspect en question. Que faites-vous?",
   option: [
     let path1 = {
-        text:"Retourner chez vous",
-        action:goToChapter("badchoice4") ,
-    },
+        text: "Retourner chez vous",
+        action: goToChapter("badchoice4"), 
+     }
     let path2 = {
         text:"Appeller votre partenaire",
         action:goToChapter("badchoice5"),
     },
+
   let path3 = {
     text:"Aller directement chez votre partenaire",
     action:goToChapter("chapitre4") ,
@@ -175,33 +180,38 @@ let badchoice5 = {
 
 let chapitre4 = {
   subtitle: "Le commencement de la mission",
+  text: "Maintenant que vous et votre partenaire sont réuni, que faites-vous?",
   option: [""],
-  img: "",
+  img: "assets/partner.jpg",
 };
+
+let badchoice6 = {
+    text: "Vous faites du spaghetti à votre partenaire mais s'étouffe et meurt",
+ }
 
 let ch4altpath = {
+  subtitle: "Commencer mission toute seule",
+  text: "Vous commencer à regrouper des informations sur votre suspect Professeur Crâne mais vous ne savez pas où rechercher les sources nécessaires",
+  option: [""],
+  img: "assets/computer.jpg",
+};
+
+
+let chapitre5 = {
   subtitle: "",
   text: "",
   option: [""],
   img: "",
 };
 
+let chapitre6 = {
+  subtitle: "",
+  text: "",
+  option: [""],
+  img: "",
+};
 
 let chapitre7 = {
-  subtitle: "",
-  text: "",
-  option: [""],
-  img: "",
-};
-
-let chapitre8 = {
-  subtitle: "",
-  text: "",
-  option: [""],
-  img: "",
-};
-
-let chapitre9 = {
   subtitle: "Le choix final",
   text: "Vous trouvez enfin professeur Crâne, sans ses hommes, sans ses armes, sans rien. Il est sans défense. Vous pointer votre revolver contre son front. Que faites-vous?",
   option: [
