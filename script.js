0fcdbc968b59fd09b4dcc66af3d0141c4b6e1f16
SparkyPX <pierrelucproulx@gmail.com>
2022-10-13 15:49:46 -0400

    Corriger projet 1

--- script.js
+++ script.js
@@ -5,124 +5,40 @@ let btn2 = document.querySelector(".dos");
 let btn3 = document.querySelector("tres");
 
 let chaptersObj = {
-  prologue: function goToChapter() {
-    console.log(prologue.subtitle, "<br>", prologue.text);
-    titre.innerText = prologue.subtitle;
-    paragraphe.innerText = prologue.text;
+  prologue: {
+    subtitle: "Introduction",
+    text:
+      "Vous incarnez une jeune adulte sous le nom de Bonnie Parker," +
+      "elle vie avec son oncle Arthur. Étant témoin du meurtre de ses parents," +
+      "elle décidèrent de poursuivre ce que son père avait commencé," +
+      "la recherche d'un homme planifiant de causer la mort de millions de personnes à travers le pays",
   },
-
-  chapitre1: function goToChapter() {
-    console.log(chapitre1.subtitle, chapitre1.text);
-    titre.innerText = chapitre1.subtitle;
-    paragraphe.innerText = chapitre1.text;
-  },
-
-  badchoice1: function goToChapter() {
-    console.log(badchoice1.text);
-    titre.innerText = prologue.subtitle;
-    paragraphe.innerText = prologue.text;
-  },
-
-  chapitre2: function goToChapter() {
-    console.log(chapitre2.subtitle, chapitre2.text);
-    titre.innerText = chapitre2.subtitle;
-    paragraphe.innerText = chapitre2.text;
-  },
-  badchoice2: function goToChapter() {
-    console.log(badchoice2.text);
-    titre.innerText = badchoice2.subtitle;
-    paragraphe.innerText = badchoice2.text;
-  },
-  badchoice3: function goToChapter() {
-    console.log(badchoice3.text);
-    titre.innerText = badchoice3.subtitle;
-    paragraphe.innerText = badchoice3.text;
-  },
-
-  chapitre3: function goToChapter() {
-    console.log(chapitre3.subtitle, chapitre3.text);
-    titre.innerText = chapitre3.subtitle;
-    paragraphe.innerText = chapitre3.text;
-  },
-
-  chapitre4: function goToChapter() {
-    console.log(chapitre4.subtitle, chapitre4.text);
-    titre.innerText = chapitre4.subtitle;
-    paragraphe.innerText = chapitre4.text;
-  },
-
-  goback: function goToChapter() {
-    console.log(goback.text);
-  },
-
-  badchoice4: function goToChapter() {
-    console.log(badchoice4.text);
-  },
-
-  badchoice5: function goToChapter() {
-    console.log(badchoice5.text);
-  },
-
-  badchoice6: function goToChapter() {
-    console.log(badchoice6.text);
-  },
-
-  ch4altpath: function goToChapter() {
-    console.log(ch4path1.subtitle, ch4path1.text);
-  },
-
-  chapitre5: function goToChapter() {
-    console.log(chapitre5.subtitle, chapitre5.text);
-  },
-
-  chapitre6: function goToChapter() {
-    console.log(chapitre6.subtitle, chapitre6.text);
-  },
-
-  badending: function goToChapter() {
-    console.log(badending.text);
-  },
-
-  goodending: function goToChapter() {
-    console.log(goodending.text);
-  },
-};
-
-let prologue = {
-  subtitle: "Introduction",
-  text:
-    "Vous incarnez une jeune adulte sous le nom de Bonnie Parker," +
-    "elle vie avec son oncle Arthur. Étant témoin du meurtre de ses parents," +
-    "elle décidèrent de poursuivre ce que son père avait commencé," +
-    "la recherche d'un homme planifiant de causer la mort de millions de personnes à travers le pays",
-};
-
-let chapitre1 = {
-  subtitle: "Le début de l'histoire",
-  text: "Il est 7h30, et vous êtes confortable dans votre lit douillet. Votre alarme sonne, que faites-vous?",
-  option: [
-    (choix1 = {
+  chapitre1 : {
+    subtitle: "Le début de l'histoire",
+    text: "Il est 7h30, et vous êtes confortable dans votre lit douillet. Votre alarme sonne, que faites-vous?",
+    img: "assets/cadran.jpg",
+    option: [{
       text: "Commander de la pizza",
-      action: goToChapter("chapitre2"),
-    }),
-    (choix2 = {
+      action: "goToChapter('chapitre2')"
+    }, {
       text: "Retourner se coucher",
       action: goToChapter("chapitre2"),
-    }),
-    (choix3 = {
+    },
+    {
       text: "Décide de faire la mission tout de suite",
       action: goToChapter("badchoice1"),
-    }),
-  ],
-  img: "assets/cadran.jpg",
+    },
+    ],
+  },
+  badchoice1 : {
+    text: "Vous décidez de faire la mission mais malheureusement, vous tombez en bas des escaliers de votre appartment et vous vous retrouviez paralysé.",
+    img: "assets/gameover1.jpg",
+  },
+
 };
 
-let badchoice1 = {
-  text: "Vous décidez de faire la mission mais malheureusement, vous tombez en bas des escaliers de votre appartment et vous vous retrouviez paralysé.",
-  img: "assets/gameover1.jpg",
-};
 
-let chapitre2 = {
+chapitre2 = {
   subtitle: "L'appel",
   text:
     "Vous recevez un appel, votre mission est de trouver professeur Crâne," +
@@ -145,7 +61,7 @@ let chapitre2 = {
   img: "assets/phonecall.jpg",
 };
 
-let altpath1 = {
+altpath1 = {
   text: "Vous prenez votre vélo mais vous n'êtes pas sur de vouloir mettre un casque de protection, voulez-vous le mettre?",
   option: [
     (yes = {
@@ -159,19 +75,20 @@ let altpath1 = {
   ],
 };
 
-let badchoice2 = {
+badchoice2 = {
   text: "Vous vous décidiez de vous y rendre en voiture, mais vous vous faites arrêter par la police pour excès de vitesse",
   img: "assets/arrest.jpg ",
 };
 
-let badchoice3 = {
+badchoice3 = {
   text: "En mettant votre casque, vous vous rendez compte qu'il était trop gros et donc vous cache la vue. Donc vous vous enfargez sur un homme armé, et il vous tire dessus",
   img: "assets/falling.jpg",
 };
 
-let chapitre3 = {
+chapitre3 = {
   subtitle: "La mission",
   text: "Vous arrivez à destination. Maintenant, vous prenez connaissance aux informations de la mission et du suspect en question. Que faites-vous?",
+  img: "assets/car.jpg",
   option: [
     (path1 = {
       text: "Retourner chez vous",
@@ -190,24 +107,24 @@ let chapitre3 = {
       action: goToChapter("ch4altpath"),
     }),
   ],
-  img: "assets/car.jpg",
 };
 
-let badchoice4 = {
+badchoice4 = {
   text:
     "Vous apprenez aux nouvelles que Professeur Crâne a mis son plan en exécution." +
     " Un grand nombre de personnes ont dû payer le prix. Dans le plus grand des remords de n'avoir rien fait, vous vous tuer.",
 };
 
-let badchoice5 = {
+badchoice5 = {
   text:
     "Quelqu'un a tracé votre numéro lorsque vous avez appellé" +
     "votre partenaire et a trouvé votre numéro d'appartement et vous kidnappe",
 };
 
-let chapitre4 = {
+chapitre4 = {
   subtitle: "Le commencement de la mission",
   text: "Maintenant que vous et votre partenaire sont réuni, que faites-vous?",
+  img: "assets/partner.jpg",
   option: [
     (choice1 = {
       text: "Lui dire bonjour",
@@ -222,22 +139,22 @@ let chapitre4 = {
       action: goToChapter("chapitre5"),
     }),
   ],
-  img: "assets/partner.jpg",
 };
 
-let goback = {
+goback = {
   text: "Billy n'a pas le temps de clavarder il vous dit de revenir plus tard. ",
   action: goToChapter("chapitre4"),
 };
 
-let badchoice6 = {
+badchoice6 = {
   text: "Vous faites du spaghetti à votre partenaire mais s'étouffe et meurt",
   img: "assets/rip.jpg",
 };
 
-let ch4altpath = {
+ch4altpath = {
   subtitle: "Commencer mission toute seule",
   text: "Vous commencer à regrouper des informations sur votre suspect Professeur Crâne mais vous ne savez pas où rechercher les sources nécessaires",
+  img: "assets/computer.jpg",
   option: [
     (addresse = {
       text: "Vous ne trouver rien à ce sujet",
@@ -248,12 +165,12 @@ let ch4altpath = {
       action: goToChapter("ch4altpath"),
     }),
   ],
-  img: "assets/computer.jpg",
 };
 
-let chapitre5 = {
+chapitre5 = {
   subtitle: "",
   text: "Après avoir pris un bon café, vous et votre partenaire discutiez d'un plan pour infiltrer la forteresse du Professeur Crâne. Après la discussion vous vous dirigiez vers sa forteresse.",
+  img: "",
   option: [
     (devant = {
       text: "Devant",
@@ -264,12 +181,12 @@ let chapitre5 = {
       action: goToChapter("chapitre6"),
     }),
   ],
-  img: "",
 };
 
-let chapitre6 = {
+chapitre6 = {
   subtitle: "Le choix final",
   text: "Vous trouvez enfin professeur Crâne, sans ses hommes, sans ses armes, sans rien. Il est sans défense. Vous pointer votre revolver contre son front. Que faites-vous?",
+  img: "",
   option: [
     (tuer = {
       text: "Tuer",
@@ -280,13 +197,18 @@ let chapitre6 = {
       action: goToChapter("goodending"),
     }),
   ],
-  img: "",
 };
 
-let badending = {
+badending = {
   text: "Vous tuer Professeur Crâne pendant qu'il ricanait, et vous sauvez l'humanité. Mais votre partenaire prise en otage meurt aussi en même temps sans vous en aperçevoir.",
 };
 
-let goodending = {
+goodending = {
   text: "Vous épargnez Professeur Crâne et mettre son plan à terme, et l'emporter à la justice. Félicitations!!!",
 };
+
+function goToChapter(chapterName) {
+
+  console.log(chapterObj[chapterName].subtitle);
+  console.log(chapterObj[chapterName].text);
+}
